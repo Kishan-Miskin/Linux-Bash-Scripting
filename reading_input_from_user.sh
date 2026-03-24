7a6b8ef8eab940895c7c9dcd91096cdbff199d79
Kishan Miskin <kishanmiskinn@gmail.com>
2026-03-24 15:12:53 +0530

    added new files

---
+++ reading_input_from_user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "What is your name : "
+read user_name
+echo "what is your age :"
+read user_age
+echo "welcome, $user_name!"
+echo "Your age is $user_age"
---
+++ variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+my_name="alice"
+my_age=22
+dept="ops"
+
+
+echo "Hello, my name is $my_name"
+echo "iam $my_age years old"
+echo "i belong to $dept department"
+
